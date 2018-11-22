5818d1913b0062bcf9aadbf9088765f94a7d1ec3
YAZILI\erdi.oran <erdi.oran@sabah.com.tr>
2018-11-22 17:58:47 +0300

    son

--- app/src/main/java/com/erdioran/efectura/MainActivity.java
+++ app/src/main/java/com/erdioran/efectura/MainActivity.java
@@ -3,25 +3,37 @@ package com.erdioran.efectura;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.android.volley.Response;
@@ -50,6 +62,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     private static final String TAG = MainActivity.class.getSimpleName();
     private FloatingActionButton fab;
 
+    private EditText editTextMail;
+    private Button cancelButton;
+    private PopupWindow mPopupWindow;
+    private FrameLayout frameLayout;
+
 
     private LocationManager locationManager;
     private GoogleApiClient mGoogleApiClient;
@@ -64,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
         recyclerView = findViewById(R.id.recycler_view);
         fab = findViewById(R.id.fab);
+        frameLayout=findViewById(R.id.frameLayout);
 
 
         fabButton();
@@ -148,17 +166,44 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
 
     public void fabButton() {
+
+        editTextMail=findViewById(R.id.editTextMail);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent email = new Intent(Intent.ACTION_SEND);
-                email.setType("text/email");
-                email.putExtra(Intent.EXTRA_EMAIL, "erdioran@gmail.com");
-                email.putExtra(Intent.EXTRA_EMAIL, "");
-                email.putExtra(Intent.EXTRA_TEXT, appVer);
-                startActivity(Intent.createChooser(email, "Send info"));
+                LayoutInflater inflater= (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View customView=inflater.inflate(R.layout.custom_layout,null);
+                mPopupWindow=new PopupWindow(customView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                mPopupWindow.setFocusable(true);
+                mPopupWindow.update();
+                if (Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                }
+
+                cancelButton=(Button)customView.findViewById(R.id.cancelBtn);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mPopupWindow.dismiss();
+                    }
+                });
+
+                mPopupWindow.showAtLocation(frameLayout,Gravity.CENTER,0,0);
             }
         });
+
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent email = new Intent(Intent.ACTION_SEND);
+//                email.setType("text/email");
+//                email.putExtra(Intent.EXTRA_EMAIL, "erdioran@gmail.com");
+//                email.putExtra(Intent.EXTRA_EMAIL, "");
+//                email.putExtra(Intent.EXTRA_TEXT, appVer);
+//                startActivity(Intent.createChooser(email, "Send info"));
+//            }
+//        });
     }
 
     public void latLog() {
